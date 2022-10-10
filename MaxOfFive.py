a31d510fdb6e15f0ca32aab462f49aaa51c15864
Rzoker <trai911505@gmail.com>
2022-10-10 11:43:23 +0300

    task 2 done hard way

--- MaxOfFive.py
+++ MaxOfFive.py
@@ -5,6 +5,9 @@
 #- 1, 4, 8, 7, 5 -> 8
 #- 78, 55, 36, 90, 2 -> 90
 
+from array import array
+
+
 list_of_nums = []
 
 for i in range(0, 5):
@@ -13,4 +16,13 @@ for i in range(0, 5):
     list_of_nums.append(number)
 
 print(list_of_nums)
-print(max(list_of_nums))
+
+
+max = list_of_nums[0]
+#print(max(list_of_nums))
+
+for i in range (len(list_of_nums)):
+    if (list_of_nums[i] > max):
+        max = list_of_nums[i]
+
+print(max)
