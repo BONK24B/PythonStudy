926705d0279e37b54f8df80c5bb79501430ff618
Rzoker <trai911505@gmail.com>
2022-10-13 03:48:17 +0300

    it finally works

---
+++ HomeworkTwo/SumOfDigits.py
@@ -0,0 +1,35 @@
+# Задача 1. Напишите программу, которая принимает на вход вещественное или целое число и показывает сумму его цифр. Через строку нельзя решать.
+
+# *Пример:*
+
+# - 6782 -> 23
+# - 0,56 -> 11
+
+
+
+    
+number = input("Enter any number: ")
+array = []
+counter = 0
+
+if("." in number):
+    number = number.split(".")
+    line = ""
+
+    for i in number:
+        line += i
+    
+    for i in line:
+        array.append(int(i))
+    
+else:
+    for i in number:
+        array.append(int(i))
+
+for i in array:
+    counter += i
+
+print(counter)
+
+
+
